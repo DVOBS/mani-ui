a2e096e5011d303b7a041102ce8fe2dfb32e232d
dvobs <1715800753@qq.com>
2021-07-22 16:16:19 +0800

    fix：修复 type 文件内容缺少

--- types/index.d.ts
+++ types/index.d.ts
@@ -2,4 +2,6 @@ import Vue from 'vue'
 
 export const InputNumber: Vue.VueConstructor<Vue>
 export const InputString: Vue.VueConstructor<Vue>
+export const InputColor: Vue.VueConstructor<Vue>
 export const InspectorItem: Vue.VueConstructor<Vue>
+export const CoordinateSystem: Vue.VueConstructor<Vue>
